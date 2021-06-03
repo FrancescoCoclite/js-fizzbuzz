3dee2739c7283c5725cfd32d42a0ba45c20ae71f
Francesco <francescococlite@gmail.com>
2021-06-03 17:19:00 +0200

    add bonus divisibilità x6 Boolean

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
-
 for (var i = 1; i <= 100; i++){
 
-    if (i % 3 == 0 &&  i % 5 == 0){
-    console.log(i + 'fizzbuzz');
+    if (i % 3 == 0 &&  i % 5 == 0 && i % 6 == 0){
+    console.log(i + 'FizzBuzzBoolean');
     }
-    else if (i % 3 == 0){
-     console.log(i + 'fizz');
+    else if (i % 3 == 0 && i % 6 == 0 ){
+        console.log(i + 'FizzBoolean');
+    }   
+    else if (i % 5 == 0 && i % 6 == 0 ){
+        console.log(i + 'BuzzBoolean');
+    }
+    else if (i % 3 == 0 ){
+     console.log(i + 'Fizz');
     }
     else if (i % 5 == 0){
-        console.log(i + 'buzz');
+        console.log(i + 'Buzz');
     }
     else{
         console.log(i);
